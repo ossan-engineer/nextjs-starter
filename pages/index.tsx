261a5dd818a2d70c069ead814ef1f703670530e8
Kiichi Tachibana <bodega1123@gmail.com>
2021-05-17 21:08:56 +0900

    Format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
-import styled from 'styled-components';
-import { Button } from '@material-ui/core';
+import styled from 'styled-components'
+import { Button } from '@material-ui/core'
 
 const Title = styled.h1`
-  color: red;
+  color: blue;
   font-size: 50px;
-`;
+`
 
 export default function Home() {
   return (
     <>
       <Title>My page</Title>
-      <Button variant='contained' disableElevation color='primary'>
+      <Button variant="contained" disableElevation color="primary">
         TEST
       </Button>
     </>
-  );
+  )
 }
