162bf6254b4c23fb68003a4396291ccfbabe2004
Kiichi Tachibana <bodega1123@gmail.com>
2021-05-17 21:08:02 +0900

    Add custom document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,66 @@
-import Document, { DocumentContext } from 'next/document'
+import React from 'react'
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps
+} from 'next/document'
+import { RenderPageResult } from 'next/dist/next-server/lib/utils'
 import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets as MaterialServerStyleSheets } from '@material-ui/core'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
+export default class CustomDocument extends NextDocument {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const styledComponentsSheet = new ServerStyleSheet()
+    const materialUiSheets = new MaterialServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
     try {
-      ctx.renderPage = () =>
+      ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+          enhanceApp:
+            (App) =>
+            (
+              props
+            ): React.ReactElement<{
+              sheet: ServerStyleSheet
+            }> =>
+              styledComponentsSheet.collectStyles(
+                materialUiSheets.collect(<App {...props} />)
+              )
         })
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await NextDocument.getInitialProps(ctx)
       return {
         ...initialProps,
-        styles: (
-          <>
+        styles: [
+          <React.Fragment key="styles">
             {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+            {styledComponentsSheet.getStyleElement()}
+            {materialUiSheets.getStyleElement()}
+          </React.Fragment>
+        ]
       }
     } finally {
-      sheet.seal()
+      styledComponentsSheet.seal()
     }
+  }
+
+  render(): React.ReactElement {
+    return (
+      <Html lang="ja-JP">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
