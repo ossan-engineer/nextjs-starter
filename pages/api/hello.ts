529c4f385d159f943b2bcbc301a046884108eceb
Kiichi Tachibana <bodega1123@gmail.com>
2021-07-02 21:59:45 +0900

    Add API routes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,6 @@
-const handler = (_, res: any) => {
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = (_: NextApiRequest, res: NextApiResponse) => {
   const body = { message: 'Hello' }
   res.statusCode = 200
   res.json(body)
