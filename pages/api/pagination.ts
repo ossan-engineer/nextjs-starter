39e0e456f48fcf4e69bb474fa14eb26a71e59c2e
Kiichi Tachibana <bodega1123@gmail.com>
2021-07-26 18:12:27 +0900

    Add page route for pagination

---
+++ pages/api/pagination.ts
@@ -0,0 +1,78 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const DATA = [
+  {
+    id: 0,
+    title: '---'
+  },
+  {
+    id: 1,
+    title: 'aaa'
+  },
+  {
+    id: 2,
+    title: 'bbb'
+  },
+  {
+    id: 3,
+    title: 'ccc'
+  },
+  {
+    id: 4,
+    title: 'ddd'
+  },
+  {
+    id: 5,
+    title: 'eee'
+  },
+  {
+    id: 6,
+    title: 'fff'
+  },
+  {
+    id: 7,
+    title: 'ggg'
+  },
+  {
+    id: 8,
+    title: 'hhh'
+  },
+  {
+    id: 9,
+    title: 'iii'
+  },
+  {
+    id: 10,
+    title: 'jjj'
+  },
+  {
+    id: 11,
+    title: 'kkk'
+  },
+  {
+    id: 12,
+    title: 'lll'
+  }
+]
+
+const userHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { index },
+    method
+  } = req
+
+  switch (method) {
+    case 'GET':
+      res
+        .status(200)
+        .json(
+          DATA.filter((d) => d.id >= Number(index) && d.id <= Number(index) + 2)
+        )
+      break
+    default:
+      res.setHeader('Allow', ['GET'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
+
+export default userHandler
