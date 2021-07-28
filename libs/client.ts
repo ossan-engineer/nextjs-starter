239d2eab0533eeeaabb3abde75dd26dd6a7da8b6
Kiichi Tachibana <bodega1123@gmail.com>
2021-07-28 17:16:26 +0900

    Fix client

--- libs/client.ts
+++ libs/client.ts
@@ -1,9 +1,6 @@
 import { createClient } from 'microcms-js-sdk'
 
-export const client =
-  process.env.NEXT_PUBLIC_SERVICE_DOMAIN &&
-  process.env.NEXT_PUBLIC_API_KEY &&
-  createClient({
-    serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN,
-    apiKey: process.env.NEXT_PUBLIC_API_KEY
-  })
+export const client = createClient({
+  serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!
+})
