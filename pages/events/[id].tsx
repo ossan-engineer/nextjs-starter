da7906da975fcd9b8c92c0148f5d744ae5c42912
Kiichi Tachibana <bodega1123@gmail.com>
2022-03-03 22:02:21 +0900

    Add EventDetailPage

--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { getEventById } from 'dummy-data'
+
+import EventSummary from '@/components/EventDetail/event-summary'
+import EventLogistics from '@/components/EventDetail/event-logistics'
+import EventContent from '@/components/EventDetail/event-content'
 
 const EventDetailPage = () => {
-  return <div>EVENT</div>
+  const router = useRouter()
+
+  const id = router.query.id as string
+  const event = getEventById(id)
+
+  if (!event) {
+    return <p>No event found!</p>
+  }
+
+  const { title, description, date, location, image } = event
+
+  return (
+    <>
+      <EventSummary title={title}></EventSummary>
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      ></EventLogistics>
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </>
+  )
 }
 
 export default EventDetailPage
